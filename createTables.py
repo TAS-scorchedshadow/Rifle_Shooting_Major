010592e0a223a8dd94e2695c7d38ac4ab39939f3
Suchpuns <suchpunsmanywow@gmail.com>
2020-12-03 12:31:48 +1100

    Add create_tables sql script file

--- createTables.py
+++ createTables.py
@@ -1,6 +1,11 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require');
+def connect():
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+
+if __name__ == '__main__':
+    connect()
