54465303eb6d17eacacc4e8bf0a842ec3000b460
scorchedshadow.mac <redman29h@gmail.com>
2020-12-04 16:52:19 +1100

    Init Commit

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -6,3 +7,17 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    stages = db.relationship('Stage', backref='shooter', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Stage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    notes = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Stage %r>' % self.notes
--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
