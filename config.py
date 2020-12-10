d7a1cf385424eb4eb4fb573c80929261c35baee7
RishiWig3 <435700969@student.sbhs.nsw.edu.au>
2020-12-10 21:32:22 +1100

    updates to the profile page

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
-print(dotenv_path)
 load_dotenv(dotenv_path)
 #basedir = os.path.abspath(os.path.dirname(__file__))
 
