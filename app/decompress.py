2015a3dd88f8d178e7c72f013aa52018c46990e1
Dylan Huynh <redman29h@gmail.com>
2021-08-10 15:37:20 +1000

    Added comments

--- app/decompress.py
+++ app/decompress.py
@@ -4,13 +4,13 @@ import tarfile
 import time
 import zlib
 import datetime
-from math import floor
 
 from app import app
 from app.models import Stage, User
 from app.uploadProcessing import checkSighter
 
-
+# Dylan Huynh
+# Compresses all zlib files at directory. Used for testing an uncompressed shot archive
 def decompress_all_zlib():
     proj = os.path.dirname(os.path.realpath('__file__'))
     uploadDir = os.path.join(proj, 'app/static/zlib_input')
@@ -25,16 +25,15 @@ def decompress_all_zlib():
         f.write(decode)
         f.close()
 
-
+# Dylan Huynh
 def decompress_zlib(file):
     decompressed_data = zlib.decompress(file)
     decoded = decompressed_data.decode("utf-8")
     return decoded
 
-
-@app.route('/archive')
+# Dylan Huynh
 def read_archive(uploaded, weeks):
-    start = time.time()
+    # start = time.time()
     # Create a list of Stage IDs present in the database
     idList = [stage.id for stage in Stage.query.all()]
     userList = [user.username for user in User.query.all()]
@@ -45,9 +44,10 @@ def read_archive(uploaded, weeks):
     new_files_in_time = 0
     stageList = []
 
-    # lastDate is the last accepted date in dattime. unixTime is the representation of that
+    # lastDate is the last accepted date in datetime. unixTime is the representation of that
     lastDate = datetime.datetime.now() - datetime.timedelta(weeks=weeks)
     unixTime = (lastDate - datetime.datetime(1970, 1, 1)).total_seconds() * 1000
+    # Mode r:gz opens in read mode for gzip
     tar = tarfile.open(mode="r:gz", fileobj=uploaded)
 
     # Find and extract "data.txt" in the archive
@@ -83,7 +83,7 @@ def read_archive(uploaded, weeks):
                         x = data_text_dict[face_id]['distance']
                         if x % 100 == 0:
                             data['distance'] = f"{x}m"
-                            print(data['distance'])
+                            #print(data['distance'])
                         else:
                             data['distance'] = f"{x}y"
                     # -- FINISH EDITING DATA --
@@ -108,14 +108,14 @@ def read_archive(uploaded, weeks):
                 # In case out of format files were added to archive e.g. "./string-default-string.zip"
                 print('invalid filename skipped')
 
-    print("Found {} files in archive".format(files_found))
-    print("Found {} new files in archive".format(new_files))
-    print("Found {} new relevant files in archive (Made in the last {} weeks)".format(new_files_in_time, weeks))
-    print(time.time()-start)
+    # print("Found {} files in archive".format(files_found))
+    # print("Found {} new files in archive".format(new_files))
+    # print("Found {} new relevant files in archive (Made in the last {} weeks)".format(new_files_in_time, weeks))
+    # print(time.time()-start)
 
     return stageList
 
-
+# Dylan Huynh
 def num_shots(data):
     num = 0
     for individualShot in data['shots']:
@@ -125,7 +125,7 @@ def num_shots(data):
                 num += 1
     return num
 
-
+# Dylan Huynh
 def check_valid_group_info(data):
     if 'stats_group_size' in data and 'stats_group_center' in data:
         return True
--- app/email.py
+++ app/email.py
@@ -4,7 +4,7 @@ from flask import render_template
 from flask_mail import Message
 from app import app, mail
 from app.models import User, Stage
-
+# Email functions adapted from examples from Flask-Mail's documentation by Dylan Huynh
 
 def send_email(subject ,recipients, text_body, html_body):
     """
@@ -14,7 +14,7 @@ def send_email(subject ,recipients, text_body, html_body):
     :param sender: Specifies the creator of the email
     :param recipients: Specifies the address of the emails
     :param text_body: Fills the body of the email
-    :param html_body: TO BE FILLED
+    :param html_body: HTML representation of the email
     :return:
     """
     msg = Message(subject, recipients=recipients)
@@ -28,7 +28,6 @@ def send_password_reset_email(user):
     Sends an email for users to reset their passwords
 
     :param user: User requesting for a change in password
-    :return: TO BE FILLED
     """
     token = user.get_reset_password_token()
     send_email('[Riflelytics] Reset Your Password',
@@ -41,7 +40,6 @@ def send_activation_email(user):
     Sends a confirmation email to the newly registered user
 
     :param user: Newly registered user
-    :return: TO BE FILLED
     """
     token = user.get_activation_token()
     send_email('Welcome to Riflelytics! Confirm Your Email',
@@ -49,8 +47,13 @@ def send_activation_email(user):
                text_body=render_template('email/activate.txt', user=user, token=token),
                html_body=render_template('email/activate.html', user=user, token=token))
 
-
+# Dylan Huynh
 def send_report_email(banned_userIDs):
+    """
+    Sends all users an email with the data from the last week. Does not send email to banned users
+
+    :param banned_userIDs: list of user ID to NOT send an email to
+    """
     users = User.query.all()
     tsNow = datetime.now()
     tsBegin = datetime.now() - timedelta(weeks=1)
