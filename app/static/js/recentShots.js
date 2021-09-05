4f86a2de8f1323078151857fce82d1fd5bcc1924
Dylan Huynh <redman29h@gmail.com>
2021-09-05 22:42:48 +1000

    Improvements to UI

--- app/static/js/recentShots.js
+++ app/static/js/recentShots.js
@@ -53,8 +53,10 @@ $(document).ready(function(){
                                                 <div class="col-4 align-self-center">
                                                     <p class="text-right" style="font-size:12px; color: black">
                                                         <a href="/target?stageID=${data[stage]['stageID']}" class="stage-view show-sheet" target="_blank">
-                                                            <span><u>Show Sheet</u></span>
-                                                            <i class="fas fa-expand-alt" style="color:black;"></i>
+                                                            <p>
+                                                                <u>View Plotsheet</u>
+                                                                <i class="fas fa-external-link-alt" style="color:black;"></i>
+                                                            </p>
                                                         </a>
                                                     </p>
                                                 </div>
