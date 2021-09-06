350758819989e5375994c36da4d77b656994d35f
Dylan Huynh <redman29h@gmail.com>
2021-09-06 14:12:09 +1000

    Error message changes

--- app/static/js/noteSubmit.js
+++ app/static/js/noteSubmit.js
@@ -2,13 +2,6 @@ $( document ).ready(function() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const stageID = urlParams.get('stageID')
-    var dict = {
-        elevation: 100,
-        foresight: 10,
-        sight: 10,
-        sling: 4
-    }
-    
     $('#notes').focus(function () {
         $("#tick").fadeOut(500);
     })
@@ -26,13 +19,4 @@ $( document ).ready(function() {
             })
         })
     });
-    // $( "#settings-button" ).click(function() {
-    //     $(this).html("<button type=\"button\" class=\"btn btn-warning btn-lg btn-block\" id=\"save-button\">\n" +
-    //         "<i class=\"fas fa-save\"></i>\n" +
-    //         "<h5>Save</h5>\n" +
-    //         "</button>")
-    //     $("#stage-table tbody tr td").each(function () {
-    //         $(this).html(`<input type='text' value=${$(this).text()}>`)
-    //     })
-    // })
 });
