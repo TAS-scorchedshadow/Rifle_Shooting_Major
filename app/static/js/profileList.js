d80ad3b8399bffc5bc72e721609d10abe5cb5f68
Henry Guo <suchpunsmanywow@gmail.com>
2022-01-31 02:07:41 +1100

    Moved profile list page code to javascript

--- app/static/js/profileList.js
+++ app/static/js/profileList.js
@@ -10,8 +10,22 @@ $(document).ready(function() {
             <h4>${users[i][0]}</h4>
         </div>
         `
-        for (let i = 0; i; i++){
+        if (users[i].length > 1) {
+            console.log(users[i])
+            for (let j = 1; j<users[i].length-1; j++){
+                console.log(users[i][j])
+                yearGroupsHtml = yearGroupsHtml +
+                `
+                <div class="pl-2 pr-2" style="display: inline-flex;">
+                    <button class="card mt-3 pl-1 pr-1" style="width:300px" type="submit" onclick="document.getElementById('idInput').value=${users[i][j][2]}">
+                        <div class="card-body" style="width:300px">
+                            <h6>${users[i][j][1]} ${users[i][j][0]} </h6>
+                        </div>
+                    </button>
+                </div>                 
+                `
 
+            }
         }
     }
     $('#yearGroups').append(yearGroupsHtml)
