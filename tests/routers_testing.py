97673c286d5ca6a1a37e1440398a47c62169308d
Dylan Huynh <redman29h@gmail.com>
2022-06-28 02:21:05 +1000

    Added template for routes.py testing

---
+++ tests/routers_testing.py
@@ -0,0 +1,21 @@
+import unittest
+from app import app, db
+
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_greeting(self):
+        self.app.get('/')
+        self.assert_template_used('index.html')
+        self.assert_context("greeting", "hello")
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
