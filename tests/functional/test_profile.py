ac234cb1000d9631cb8f9825acdf76da270b5107
Dylan Huynh <dylan.huynh2401@gmail.com>
2022-07-04 00:38:21 +1000

    Removed post test

--- tests/functional/test_profile.py
+++ tests/functional/test_profile.py
@@ -8,13 +8,3 @@ def test_get_target_stats_get(test_client):
     data = '0'
     response = test_client.get('/get_target_stats')
     assert 405 == response.status_code
-
-
-def test_get_target_stats_post(test_client):
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/get_target_stats' page is requested (GET)
-    THEN check that the response is valid
-    """
-    response = test_client.post('/testPost', data={'name': 'flask'})
-    assert 'hello' == response.data.decode("utf-8")
