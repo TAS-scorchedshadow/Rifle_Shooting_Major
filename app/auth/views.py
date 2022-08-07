4040f459b48509bd5f65da5d1a7f2f5ad232ac12
Suchpuns <suchpunsmanywow@gmail.com>
2022-08-08 01:20:41 +1000

    Removed unused print statements

--- app/auth/views.py
+++ app/auth/views.py
@@ -24,8 +24,6 @@ def login():
     if current_user.is_authenticated:
         return redirect(url_for('welcome_bp.index'))
     form = signInForm()
-    generate_csrf()
-    print(flask_session)
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
