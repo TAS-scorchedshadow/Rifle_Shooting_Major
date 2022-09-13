4ee73e1a12bb73af2352e33754c0d7fea5f1dcbb
Dylan <redman29h@gmail.com>
2022-09-13 16:31:53 +1000

    Updating gitlab ci

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,7 +16,6 @@ class TestingConfig(Config):
     WTF_CSRF_ENABLED = False
     MAIL_SUPPRESS_SEND = True
     FORCE_HTTPS = False
-    SECRET_KEY = "SUPER_SECRET"
     SQLALCHEMY_DATABASE_URI = "sqlite:///"
 
 
