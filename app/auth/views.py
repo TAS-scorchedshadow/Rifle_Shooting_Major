4db3743570b175431638766cbaedf42bf3cfc4f5
Dylan Huynh <redman29h@gmail.com>
2022-09-17 18:09:22 +1000

    Made registration club-sensitive

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, redirect, url_for, flash, request, render_template
+from flask import Blueprint, redirect, url_for, flash, request, render_template, abort
 from flask_login import current_user, login_user, logout_user
 from flask import session as flask_session
 from flask_wtf.csrf import generate_csrf
 from werkzeug.urls import url_parse
 
 from app import db
-from app.models import User
+from app.models import User, Club
 
 from .forms import signInForm, signUpForm, independentSignUpForm, ResetPasswordRequestForm, ResetPasswordForm
 from .email import send_activation_email, send_password_reset_email
@@ -48,10 +48,11 @@ def register():
     if form.validate_on_submit():
         email = form.schoolID.data + "@student.sbhs.nsw.edu.au"
         user = User(fName=form.fName.data.strip().lower().title(), sName=form.sName.data.strip().lower().title(),
-                    school=form.school.data,
                     schoolID=form.schoolID.data, email=email, gradYr=str(form.gradYr.data))
         user.generate_username()
         user.set_password(form.password.data)
+        club = Club.query.filter_by(name=form.school.data).first()
+        user.clubID = club.id
         db.session.add(user)
         db.session.commit()
         send_activation_email(user)
