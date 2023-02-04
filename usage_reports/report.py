24d7b36bf51b6176221aff50ffc2991ad76bba14
Dylan <redman29h@gmail.com>
2023-02-04 13:59:26 +1100

    Added logos and changes active threshold field

--- usage_reports/report.py
+++ usage_reports/report.py
@@ -6,6 +6,8 @@ from app.models import Club, User
 
 from app import create_app
 
+from fpdf import FPDF
+
 
 def run_generate_usage_report():
     app = create_app()
@@ -23,7 +25,13 @@ def run_generate_usage_report():
 
 
 def generate_usage_report(club_name: str, start: str, end: str, num_free_stages: int):
-
+    """
+    Generates a json.txt file. File contains stage & shot data for all members in a club
+    :param club_name: Club name must match database
+    :param start: YYYY-MM-DD
+    :param end: YYYY-MM-DD
+    :param num_free_stages: Minimum number of stages for a user to be active
+    """
     club = Club.query.filter_by(name=club_name).first()
     if not club:
         raise ValueError("No club of that name was found")
@@ -44,7 +52,7 @@ def generate_usage_report(club_name: str, start: str, end: str, num_free_stages:
             "club_name": club.name,
             "start": start,
             "end": end,
-            "active_threshold": f'At least {num_free_stages} stages',
+            "active_threshold": num_free_stages,
             "num_active_users": 0,
             "num_inactive_users": 0,
         },
@@ -70,7 +78,7 @@ def generate_usage_report(club_name: str, start: str, end: str, num_free_stages:
             data["inactive_users"].append(u_usage_data)
             data["club_info"]["num_inactive_users"] += 1
 
-        print(f"{searched}/{num_users}  {int(searched/num_users * 100)}%")
+        print(f"{searched}/{num_users}  {int(searched / num_users * 100)}%")
         searched += 1
     json_object = json.dumps(data, indent=4)
 
@@ -78,7 +86,37 @@ def generate_usage_report(club_name: str, start: str, end: str, num_free_stages:
     print(f"Writing to {output_name}")
     with open(output_name, "w") as outfile:
         outfile.write(json_object)
+def json_to_pdf():
+    class PDF(FPDF):
+        def header(self):
+            # logo
+            self.image('../app/static/logo/logo-text.png', 10, 8, 25)
+            # font
+            self.set_font('helvetica', 'B', 20)
+            # Padding
+            self.cell(80)
+            # Title
+            self.cell(100, 10, 'Usage Data', border=True, ln=1, align='C')
+
+            self.set_font('helvetica', '', 14)
+            self.ln(1)
+            self.cell(95)
+            self.cell(70, 10, '2022-01-01 to 2022-12-12', border=False, ln=1, align='C')
+            # Line Break
+            self.ln(10)
+
+    pdf = PDF('P', 'mm', "A4")
+    pdf.set_auto_page_break(auto=True, margin=15)
+
+    pdf.add_page()
+
+    pdf.set_font('times', '', 12)
+
+    for i in range(1, 41):
+        pdf.cell(0, 10, f'This is line{i} :D', ln=True)
+
+    pdf.output('pdf_2.pdf')
 
 
 if __name__ == "__main__":
-    run_generate_usage_report()
+    json_to_pdf()
