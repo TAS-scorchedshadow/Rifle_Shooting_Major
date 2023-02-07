a395b0269a9d6e719384867cb0e0d3cb403de0ef
Dylan <redman29h@gmail.com>
2023-02-07 14:08:19 +1100

    Profile list now hides empty years & displays graduated students

--- app/static/js/profile_list.js
+++ app/static/js/profile_list.js
@@ -4,35 +4,35 @@ $(document).ready(function() {
     let yearGroupDiv = document.getElementById('yearGroups')
     let yearGroupsHtml = ''
     for (let i = 12; i > 6; i--) {
-        yearGroupsHtml = yearGroupsHtml +
-        `
-        <div id="${users[i][0]}" class="mt-3">
-            <h4>${users[i][0]}</h4>
-        </div>
-        `
-        if (users[i].length > 1) {
-            for (let j = 1; j<users[i].length; j++){
+        yearGroupsHtml = appendUsers(yearGroupsHtml, users, i)
+    }
+    yearGroupsHtml = appendUsers(yearGroupsHtml, users, 'other')
+    console.log(yearGroupsHtml)
+    $('#yearGroups').append(yearGroupsHtml)
+})
+
+function appendUsers(yearGroupsHtml, users, year) {
+        if (users[year].length > 1) {
+            yearGroupsHtml = yearGroupsHtml +
+            `
+            <div id="${users[year][0]}" class="mt-3">
+                <h4>${users[year][0]}</h4>
+            </div>
+            `
+            for (let j = 1; j < users[year].length; j++){
                 yearGroupsHtml = yearGroupsHtml +
                 `
                 <div class="pl-2 pr-2" style="display:inline-block; align-self: center;">
-                    <button class="card mt-3 pl-1 pr-1" style="width:300px; display:inline;" type="submit" onclick="document.getElementById('idInput').value=${users[i][j][2]}">
+                    <button class="card mt-3 pl-1 pr-1" style="width:300px; display:inline;" type="submit" onclick="document.getElementById('idInput').value=${users[year][j][2]}">
                         <div class="card-body" style="width:300px">
-                            <h6>${users[i][j][1]} ${users[i][j][0]} </h6>
+                            <h6>${users[year][j][1]} ${users[year][j][0]} </h6>
                         </div>
                     </button>
                 </div>                 
                 `
 
             }
+            yearGroupsHtml += "<hr>"
         }
-        else {
-            yearGroupsHtml = yearGroupsHtml +
-                `
-                No shooters
-                `
-        }
-        yearGroupsHtml = yearGroupsHtml + "<hr>"
-    }
-    console.log(yearGroupsHtml)
-    $('#yearGroups').append(yearGroupsHtml)
-})
+    return yearGroupsHtml
+}
